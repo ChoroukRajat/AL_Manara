e816187283edbc79a7f62b1795053f728018c18b
ChoroukRajat <165086687+ChoroukRajat@users.noreply.github.com>
2024-12-08 11:27:22 +0100

    Add files via upload

---
+++ Almanara_app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "./hero";
+import Posts from "./posts";
+import Articles from "./articles";
+
+export default function Campaign() {
+    return (
+        <>
+            <Hero />
+
+        </>
+    );
+}
